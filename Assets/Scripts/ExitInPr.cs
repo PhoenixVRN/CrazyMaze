b05be737cb2deb4be7555744c3ab454dc1d30064
PhoenixVRN <projectfenixru@gmail.com>
2022-01-18 20:28:26 +0300

    Рабочий прототип!!!

---
+++ Assets/Scripts/ExitInPr.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class ExitInPr : MonoBehaviour
+{
+   public void Exit()
+   {
+      Application.Quit();
+   } 
+}
