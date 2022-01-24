8b20591ed6e468388c22680b4775ced7e3495031
EvgenyiVRN <projectfenixru@gmail.com>
2022-01-24 17:33:30 +0300

    Закостылили

--- Assets/Scripts/Inventory/Pickup.cs
+++ Assets/Scripts/Inventory/Pickup.cs
@@ -8,28 +8,37 @@ public class Pickup : MonoBehaviour
     private Inventory _Inventory;
     public GameObject slotButton;
     public GameObject exit;
+    public GameObject exitView;
+    public GameObject view;
 
     private void Start()
     {
      var ddd = GameObject.FindGameObjectsWithTag("Player");
      _Inventory = ddd[0].GetComponent<Inventory>();
+     Instantiate(slotButton, _Inventory.slots[0].transform);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            for (int i = 0; i < _Inventory.slots.Length; i++)
-            {
-                if (_Inventory.isFull[i] == false)
-                {
-                    _Inventory.isFull[i] = true;
+ //           for (int i = 0; i < _Inventory.slots.Length; i++)
+//            {
+//                if (_Inventory.isFull[i] == false)
+//                {
+//                    _Inventory.isFull[i] = true;
+
                     exit.SetActive(true);
-                    Instantiate(slotButton, _Inventory.slots[i].transform);
+                    foreach (Transform child in view.transform)
+                    {
+                        GameObject.Destroy((child.gameObject));
+                    }
+                   
+                    Instantiate(exitView, _Inventory.slots[0].transform);
                     Destroy(gameObject);
-                    break;
-                }
-            }
+//                    break;
+//                }
+ //           }
         }
     }
 }
