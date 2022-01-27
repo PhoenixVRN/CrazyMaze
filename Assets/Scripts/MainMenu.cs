a1817c8c35c72b0b8a5d3e669f443d05bc91f292
EvgenyiVRN <projectfenixru@gmail.com>
2022-01-27 11:35:33 +0300

    1.27.1.2

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,17 @@
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
   public void PlayGame()
   {
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    SceneManager.LoadScene(1);
   }
 
-  public void ExitGame()
-  {
-    Application.Quit();
-  }
+   public void ExitGame()
+   {
+     Application.Quit();
+   }
 }
